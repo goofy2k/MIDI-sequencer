3322a90401002a37874ad53352514dbee85e95f2
Goofy2k <goofy2k@gmail.com>
2022-02-13 12:18:05 +0100

    fresh version for next steps

---
+++ fckx_sequencer_v34_NEW_RtMidi_nimBLEOut/components/NiCMidi/src/notifier.cpp
@@ -0,0 +1,183 @@
+/*
+ *   NiCMidi - A C++ Class Library for MIDI
+ *
+ *   Copyright (C) 2004  J.D. Koftinoff Software, Ltd.
+ *   www.jdkoftinoff.com jeffk@jdkoftinoff.com
+ *   Copyright (C) 2021  Nicola Cassetta
+ *   https://github.com/ncassetta/NiCMidi
+ *
+ *   This file is part of NiCMidi.
+ *
+ *   NiCMidi is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   NiCMidi is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NiCMidi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "../include/notifier.h"
+#include "../include/sequencer.h"
+
+
+const char MIDISequencerGUIEvent::group_names[][10] =
+        { "All      ", "Conductor", "Transport", "Track    ", "Recorder ", "User     " };
+const char MIDISequencerGUIEvent::conductor_items_names[][10] =
+        { "Tempo    ", "Timesig  ", "Keysig   ", "Marker   ", "User     " };
+const char MIDISequencerGUIEvent::transport_items_names[][10] =
+        { "Start    ", "Stop     ", "Measure  ", "Beat     ", "Count in ", "User     " };
+const char MIDISequencerGUIEvent::track_items_names[][10] =
+        { "Name     ", "Program  ", "Note     ", "Volume   ", "Pan      ", "Chorus   ", "Reverb   ", "User     " };
+const char MIDISequencerGUIEvent::recording_items_names[][10] =
+        { "Reset    ", "Start    ", "Stop     ", "User     " };
+const char MIDISequencerGUIEvent::user_items_names[][10] =
+        { "User     " };
+
+bool MIDISequencerGUINotifierText::SetStartFrom(char f) {
+    if (f == 0 || f == 1) {
+        start_from = f;
+        return true;
+    }
+    return false;
+}
+
+
+void MIDISequencerGUINotifierText::Notify(const MIDISequencerGUIEvent &ev) {
+// reworked with an unique call to ost <<, so that there's no trouble with
+// cout call in other threads. (Crashed???)
+    if (sequencer == 0) return;
+    if (!en) return;                    // not enabled
+
+    char s[200];
+    int trk_num = ev.GetSubGroup();     // used only for track events
+    int wr = sprintf(s, "GUI EVENT: %s ", MIDISequencerGUIEvent::group_names[ev.GetGroup()]);
+
+    switch(ev.GetGroup()) {
+        case MIDISequencerGUIEvent::GROUP_ALL:
+            sprintf(s + wr, "GENERAL RESET");
+            break;
+        case MIDISequencerGUIEvent::GROUP_CONDUCTOR:
+            switch (ev.GetItem()) {
+                case MIDISequencerGUIEvent::GROUP_CONDUCTOR_TEMPO:
+                    sprintf(s + wr, "TEMPO:    %2f bpm", sequencer->GetState()->tempobpm);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_CONDUCTOR_TIMESIG:
+                    sprintf(s + wr, "TIMESIG:  %d/%d", sequencer->GetState()->timesig_numerator,
+                           sequencer->GetState()->timesig_denominator);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_CONDUCTOR_KEYSIG:
+                    sprintf(s + wr, "KEYSIG:   %s", KeyName(sequencer->GetState()->keysig_sharpflat,
+                                                       sequencer->GetState()->keysig_mode));
+                    break;
+                case MIDISequencerGUIEvent::GROUP_CONDUCTOR_MARKER:
+                    sprintf(s + wr, "MARKER:   %s", sequencer->GetState()->marker_text.c_str());
+                    break;
+                case MIDISequencerGUIEvent::GROUP_CONDUCTOR_USER:
+                    sprintf(s + wr, "USER EV Item %d", ev.GetItem());
+                    break;
+            }
+            break;
+        case MIDISequencerGUIEvent::GROUP_TRANSPORT:
+            switch (ev.GetItem()) {
+                case MIDISequencerGUIEvent::GROUP_TRANSPORT_START:
+                    sprintf(s + wr, "SEQUENCER START");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRANSPORT_STOP:
+                    sprintf(s + wr, "SEQUENCER STOP");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRANSPORT_MEASURE:
+                    sprintf(s + wr, "MEAS %d", sequencer->GetCurrentMeasure() + start_from);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRANSPORT_BEAT:
+                    sprintf(s + wr, "MEAS %d BEAT %d", sequencer->GetCurrentMeasure() + start_from,
+                           sequencer->GetCurrentBeat() + start_from );
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRANSPORT_COUNTIN:
+                    sprintf(s + wr, "SEQUENCER COUNT IN");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRANSPORT_USER:
+                    sprintf(s + wr, "USER EV Item %d", ev.GetItem());
+                    break;
+            }
+            break;
+        case MIDISequencerGUIEvent::GROUP_TRACK:
+            wr += sprintf (s + wr, "TRACK %3d ", trk_num);
+            switch (ev.GetItem()) {
+                case MIDISequencerGUIEvent::GROUP_TRACK_NAME:
+                    sprintf(s + wr, "NAME: %s", sequencer->GetTrackState(trk_num)->track_name.c_str());
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_PROGRAM:
+                    sprintf(s + wr, "PROGRAM: %d", sequencer->GetTrackState(trk_num)->program);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_NOTE:
+                    sprintf(s + wr, "NOTE");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_VOLUME:
+                    sprintf(s + wr, "VOLUME: %d",sequencer->GetTrackState(trk_num)->control_values[C_MAIN_VOLUME]);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_PAN:
+                    sprintf(s + wr, "PAN: %d", sequencer->GetTrackState(trk_num)->control_values[C_PAN]);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_CHR:
+                    sprintf(s + wr, "CHORUS: %d", sequencer->GetTrackState(trk_num)->control_values[C_CHORUS_DEPTH]);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_REV:
+                    sprintf(s + wr, "REVERB: %d", sequencer->GetTrackState(trk_num)->control_values[C_EFFECT_DEPTH]);
+                    break;
+                case MIDISequencerGUIEvent::GROUP_TRACK_USER:
+                    sprintf(s + wr, "USER EV Item %d", ev.GetItem());
+                    break;
+            }
+            break;
+        case MIDISequencerGUIEvent::GROUP_RECORDER:
+            switch (ev.GetItem()) {
+                case MIDISequencerGUIEvent::GROUP_RECORDER_RESET:
+                    sprintf(s + wr, "RECORDER RESET");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_RECORDER_START:
+                    sprintf(s + wr, "RECORDING START");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_RECORDER_STOP:
+                    sprintf(s + wr, "RECORDING STOP");
+                    break;
+                case MIDISequencerGUIEvent::GROUP_RECORDER_USER:
+                    sprintf(s + wr, "USER EV Item %d", ev.GetItem());
+                    break;
+            }
+            break;
+        case MIDISequencerGUIEvent::GROUP_USER:
+            sprintf(s + wr, "Subgroup: %d Item: %d", ev.GetSubGroup(), ev.GetItem());
+            break;
+    }
+    strcat(s, "\n");
+    ost << s;
+}
+
+
+
+#ifdef _WIN32
+
+MIDISequencerGUINotifierWin32::MIDISequencerGUINotifierWin32 (
+    HWND w, DWORD msg, WPARAM param_value ) :
+    dest_window ( w ), window_msg ( msg ), wparam_value ( param_value ) {}
+
+
+// NEW BY NC: auto sets window_msg and wparam_value
+MIDISequencerGUINotifierWin32::MIDISequencerGUINotifierWin32 ( HWND w ) :
+    dest_window ( w ), window_msg ( GetSafeSystemMsgId() ), wparam_value ( 0 ) {}
+
+
+void MIDISequencerGUINotifierWin32::Notify (const MIDISequencerGUIEvent &ev) {
+    if ( en )
+        PostMessage (dest_window, window_msg, wparam_value, (unsigned long) ev);
+}
+
+
+#endif // _WIN32
---
+++ fckx_sequencer_v34_NEW_RtMidi_nimBLEOut/components/NiCMidi/src/thru.cpp
@@ -0,0 +1,247 @@
+/*
+ *   NiCMidi - A C++ Class Library for MIDI
+ *
+ *   Copyright (C) 2004  J.D. Koftinoff Software, Ltd.
+ *   www.jdkoftinoff.com jeffk@jdkoftinoff.com
+ *   Copyright (C) 2021  Nicola Cassetta
+ *   https://github.com/ncassetta/NiCMidi
+ *
+ *   This file is part of NiCMidi.
+ *
+ *   NiCMidi is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   NiCMidi is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NiCMidi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "../include/thru.h"
+#include "../include/manager.h"
+#include "esp_log.h"
+
+static const char *TAG = "MIDIThru";  
+
+MIDIThru::MIDIThru() : MIDITickComponent(PR_PRE_SEQ, StaticTickProc), in_port(0), out_port(0), in_channel(-1),
+                                         out_channel(-1), processor(0)
+{
+    std::cout << "DEBUG MIDIThru constructor" << std::endl;
+    //check if in and out ports exist
+    if (!MIDIManager::IsValidInPortNumber(0) || !MIDIManager::IsValidOutPortNumber(0))
+        throw RtMidiError("MIDIThru needs almost a MIDI in and out port in the system\n", RtMidiError::INVALID_DEVICE);
+}
+
+
+MIDIThru::~MIDIThru() {
+    //std::cout << "MIDIThru destructor" << std::endl;
+    Stop();
+}
+
+
+
+void MIDIThru::Reset() {
+    Stop();
+    SilentOut();
+    in_port = 0;
+    out_port = 0;
+    processor = 0;
+    in_channel = -1;
+    out_channel = -1;
+}
+
+
+bool MIDIThru::SetInPort(unsigned int port) {
+    if (!MIDIManager::IsValidInPortNumber(port))
+        return false;                               // avoids out of range errors
+    if (port == in_port)
+        return true;                                // trying to assign same ports: nothing to do
+
+    if (IsPlaying()) {
+        proc_lock.lock();
+        MIDIManager::GetInDriver(in_port)->ClosePort();
+        SilentOut();
+        MIDIManager::GetInDriver(port)->OpenPort();
+        in_port = port;
+        proc_lock.unlock();
+    }
+    else
+        in_port = port;
+    return true;
+}
+
+
+bool MIDIThru::SetOutPort(unsigned int port) {
+    if (!MIDIManager::IsValidOutPortNumber(port))
+        return false;                               // avoids out of range errors
+    if (port == out_port)
+        return true;                                // trying to assign same ports: nothing to do
+
+    if (IsPlaying()) {
+        proc_lock.lock();
+        SilentOut();
+        MIDIManager::GetOutDriver(out_port)->ClosePort();
+        MIDIManager::GetOutDriver(port)->OpenPort();
+        out_port = port;
+        proc_lock.unlock();
+    }
+    else
+        out_port = port;
+    return true;
+}
+
+
+void MIDIThru::SetProcessor(MIDIProcessor* proc) {
+    if (IsPlaying()) {
+        proc_lock.lock();
+        SilentOut();
+        processor = proc;
+        proc_lock.unlock();
+    }
+    else
+        processor = proc;
+}
+
+
+bool MIDIThru::SetInChannel(char chan) {
+    if (chan < -1 || chan > 15)                     // avoids out of range errors
+        return false;
+    if (IsPlaying()) {
+        proc_lock.lock();
+        SilentOut();
+        in_channel = chan;
+        proc_lock.unlock();
+    }
+    else
+        in_channel = chan;
+    return true;
+}
+
+
+bool MIDIThru::SetOutChannel(char chan) {
+    if (chan < -1 || chan > 15)                     // avoids out of range errors
+        return false;
+
+    if (IsPlaying()) {
+        proc_lock.lock();
+        SilentOut();
+        out_channel = chan;
+        proc_lock.unlock();
+    }
+    else
+        out_channel = chan;
+    return true;
+}
+
+
+void MIDIThru::Start() {
+    if (!IsPlaying()) {
+        MIDIManager::GetInDriver(in_port)->OpenPort(); //FCKX
+        MIDIManager::GetOutDriver(out_port)->OpenPort();
+        MIDITickComponent::Start();
+    }
+}
+
+
+void MIDIThru::Stop() {
+    if (IsPlaying()) {
+        MIDITickComponent::Stop();
+        MIDIManager::GetInDriver(in_port)->ClosePort();
+        SilentOut();
+        MIDIManager::GetOutDriver(out_port)->ClosePort();
+    }
+}
+
+
+void MIDIThru::SilentOut() {
+    if (out_channel != -1)
+        MIDIManager::GetOutDriver(out_port)->AllNotesOff(out_channel);
+    else
+        MIDIManager::GetOutDriver(out_port)->AllNotesOff();
+}
+
+
+void MIDIThru::StaticTickProc(tMsecs sys_time, void* pt) {
+    MIDIThru* thru_pt = static_cast<MIDIThru *>(pt);
+    thru_pt->TickProc(sys_time);
+}
+
+
+void MIDIThru::TickProc(tMsecs sys_time_)
+{
+   static const char *TAG = "MIDIThru::TickProc";
+   //ESP_LOGE(TAG,"THRU TICK POS1" );
+   proc_lock.lock();
+   //proc_lock.try_lock(); //FCKX
+   MIDIInDriver* in_driver = MIDIManager::GetInDriver(in_port);
+   in_driver->LockQueue();
+    /*
+    static unsigned int times = 0;
+    times++;
+    if (!(times % 100)) 
+        std::cout << "FCKX MIDIThru::TickProc() called " << times << " times\n";
+    std::cout << "FCKX MIDIManager::GetNumMIDIIns(): " << MIDIManager::GetNumMIDIIns() << "\n";
+    */
+    MIDIRawMessage rmsg;
+    MIDITimedMessage msg;
+    //std::cout << "in_port" << in_port << "\n";
+
+    MIDIOutDriver* out_driver = MIDIManager::GetOutDriver(out_port);
+
+    //std::cout << "FCKX MIDIThru::TickProc() inspects input queue: size ="<<in_driver->GetQueueSize()<<"\n";
+if (in_driver->GetQueueSize() > 0)
+    for (unsigned int i = 0; i < in_driver->GetQueueSize(); i++) {
+        std::cout << "Message found\n";
+        //get message from the queue WITHOUT deleting it. This is done by Manager at the end of the MidiTicks queue
+        in_driver->ReadMessage(rmsg, i);
+        msg = rmsg.msg;
+     //   ESP_LOGE(TAG,"msg.GetLength() %d", msg.GetLength() );       
+  /* //can not access bytes per bytenr as GetByten is hard coded  //useful for longer RPN/NRPN messages
+     for (unsigned int ii = 0; ii < msg.GetLength(); ii++) {
+      
+            
+        }
+  */
+        ESP_LOGD(TAG,"length %d msg.Status() %u (0x%X) B1 %u (0x%X) B2 %u (0x%X) B3 %u (0x%X)", msg.GetLength(), msg.GetStatus(), msg.GetStatus(), msg.GetByte1(), msg.GetByte1(), msg.GetByte2(), msg.GetByte2(), msg.GetByte3(), msg.GetByte3());
+        //ESP_LOGD(TAG,"msg.GetByte1() %u 0x%X", msg.GetByte1(), msg.GetByte1());
+        //ESP_LOGD(TAG,"msg.GetByte2() %u 0x%X", msg.GetByte2(), msg.GetByte2());  
+        //ESP_LOGD(TAG,"msg.GetByte3() %u 0x%X", msg.GetByte3(), msg.GetByte3()); 
+        /*
+        std::cout << "msg.GetLength()"<< msg.GetLength() <<"\n"; 
+        std::cout << "msg.GetStatus()"<< msg.GetStatus() <<"\n"; 
+        std::cout << "msg.GetByte1()"<< msg.GetByte1() <<"\n"; 
+        std::cout << "msg.GetByte2()"<< msg.GetByte2() <<"\n"; 
+        std::cout << "msg.GetByte3()"<< msg.GetByte3() <<"\n";          
+        */
+        //ESP_LOGE(TAG,"msg.IsChannelMsg() %d", msg.IsChannelMsg()); 
+
+        ESP_LOGD(TAG,"msg.IsChannelMsg() %d", msg.IsChannelMsg()); 
+   
+        if (msg.IsChannelMsg()) {
+                ESP_LOGD(TAG,"in_channel %d out_channel %d msg.GetChannel() %d ",in_channel, out_channel, msg.GetChannel());     
+                if (in_channel == msg.GetChannel() || in_channel == -1) { //FCKX!!
+                //if (in_channel == msg.GetChannel() || in_channel == -1) {
+                    if (out_channel != -1) { //FCKX!!
+                    //if (out_channel != -1) {
+                        msg.SetChannel(out_channel);
+                        //std::cout << "MIDIThru::TickProc out_channel != -1\n";
+                        }
+                    if (processor) {
+                        processor->Process(&msg); 
+                        //std::cout << "MIDIThru::TickProc processor\n";                   
+                        }
+                    //std::cout << "MIDIThru::TickProc BEFORE out_driver->OutputMessage(msg) \n";      
+                    out_driver->OutputMessage(msg);
+                    //std::cout << "MIDIThru::TickProc AFTER out_driver->OutputMessage(msg) \n";                  
+                }
+            } else {ESP_LOGE(TAG,"NOT A CHANNEL MESSAGE"); }
+        }
+    in_driver->UnlockQueue();
+    proc_lock.unlock();
+}
